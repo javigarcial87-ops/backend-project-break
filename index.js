e67532c4bde5ecc2cdc7dd1c0c2adaf741216723
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-05 21:09:53 +0100

    configurar servidor express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //index.js
+require("dotenv").config()
+const express = require("express")
+const connectDB = require("./config/db")
+
+const productRoutes = require("./routes/productRoutes")
+
+const app = express()
+
