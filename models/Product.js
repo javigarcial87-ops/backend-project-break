b3daa7f7206da677531d822d17b4e36cb467585e
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-05 21:00:52 +0100

    rt

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,42 @@
 //Product.js
+const mongoose = requiere("mongoose")
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type:String,
+            required:true,
+        },
+
+        description:{
+            type:String,
+            required: true,
+        },
+
+        image: {
+            type:String,
+            required:true,
+        },
+
+        category: {
+            Type:String,
+            enum: ["Camisetas", "Pantalones", "Zapatos", "Abrigos"],
+            required: true,
+        },
+
+        size: {
+            type:String,
+            enum: ["XS","S","M", "L", "XL"],
+            required: true,
+
+        },
+
+        price: {
+            type: Number,
+            required: true,
+        },
+    },
+    {timestamps: true}
+) 
+
+module.exports = mongoose.model("Product", productSchema.Schema)
