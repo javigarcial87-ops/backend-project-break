ce44dd1a1f0c32f30377415767bcddb51273fbe7
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-10 15:48:50 +0100

    comprobacion de conexion al servidor

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 //index.js
-require("dotenv").config()
-const express = require("express")
-const connectDB = require("./config/db")
-const methodOverride = require("method-override")
-const productRoutes = require("./routes/productRoutes")
+require('dotenv').config()
+const express = require('express')
+const methodOverride = require('method-override')
+const connectDB = require('./config/db')
+
+const productRoutes = require('./routes/productRoutes')
 
 const app = express()
+
+connectDB()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.use(methodOverride("_method"))
+app.use(methodOverride('_method'))
 
-connectDB()
 
-app.use("/", productRoutes)
+
+app.use('/', productRoutes)
 
 const PORT = process.env.PORT || 3000
 
