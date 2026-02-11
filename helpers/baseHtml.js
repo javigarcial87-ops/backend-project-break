0fdc5cf64213fb157a12bf8a7382993ea764e20c
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-11 19:07:42 +0100

    correcciones de tipado

--- helpers/baseHtml.js
+++ helpers/baseHtml.js
@@ -1,9 +1,11 @@
 
 const baseHtml = (content) => `
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-  <title>Tienda</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
   <style>
     body { font-family: Arial; padding: 20px; }
     .card { border: 1px solid #ccc; padding: 10px; margin: 10px; }
