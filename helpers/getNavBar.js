0bff53e21d2ca49027f4de885cd0a712f71f3780
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-12 20:42:36 +0100

    creacion getNavBar

---
+++ helpers/getNavBar.js
@@ -0,0 +1,18 @@
+const getNavBar = (isDashboard = false) => {
+    return `
+        <nav style="margin-bottom:20px;">
+            <a href="/products">Tienda</a>
+            <a href="/dashboard">Dashboard</a>
+             ${
+                isDashboard
+                    ? ' |<a href="/dashboard/new">Nuevo producto</a>
+                    : ""
+    }
+</nav>
+<hr>
+
+    `
+}
+
+
+module.exports = getNavBar
