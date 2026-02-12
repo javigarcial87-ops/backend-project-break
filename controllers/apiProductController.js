c9b8812d60b0de1bca693266422353425edff7d8
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-12 18:10:08 +0100

    api  json

---
+++ controllers/apiProductController.js
@@ -0,0 +1,53 @@
+const product = require("../models/Product")
+//mostrar productos
+const getProducts = async (req,res) => {
+    try {
+        const products = await Product.find()
+        res.json(products)
+    }   catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+//buscar producto por id
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
+//crear productos
+const getProductsById = async (req,res) => {
+    try {
+        const newProduct = await Product.create(req.body)
+        res.status(201).json(newProduct)
+    }   catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
+
+
+//actualizar producto
+const updateProductApi = async(req,res)=> {
+    try {
+        const updated = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        )
+        res.json(updated)
+    } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
+
+
+
+
+
