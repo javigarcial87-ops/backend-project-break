f96f5b167422c9c6971100b2a1e32331d0779f31
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-12 20:13:17 +0100

    correcciones

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB conectado");
+    console.log("MongoDB se ha conectado con éxito");
   } catch (error) {
     console.error("Error conectando a MongoDB:", error);
     process.exit(1);
